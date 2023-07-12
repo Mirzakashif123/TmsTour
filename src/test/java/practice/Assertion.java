76af07f9650eb7e77c118f6a0fd8ed40f029c611
Mirzakashif123 <129642891+Mirzakashif123@users.noreply.github.com>
2023-07-12 15:38:56 +0530

    Update Assertion.java
    ss

--- src/test/java/practice/Assertion.java
+++ src/test/java/practice/Assertion.java
@@ -6,7 +6,7 @@ import static org.testng.Assert.assertTrue;
 
 import org.testng.Reporter;
 import org.testng.annotations.Test;
-//welcome
+//welcomeftg
 public class Assertion {
 	@Test
 	public void checkTrue() {
